936786ffa73fc331eb8768884cf2031488487684
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:38:02 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/conanport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/conanport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/conanport.git'), string(name: 'PORT_DESCRIPTION', value: 'The open-source C and C++ package manager' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/conanport.git'), string(name: 'PORT_DESCRIPTION', value: 'The open-source C and C++ package manager' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
